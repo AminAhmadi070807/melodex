9f507a84d0df0acbfef6bd2aa3fd44c0036739ac
Amin Ahmadi <aminahmadi070807@gmail.com>
2025-11-15 06:48:27 +0330

    feat: enable updateting music in list.

--- src/modules/v1/musics/mp3/music.controller.js
+++ src/modules/v1/musics/mp3/music.controller.js
@@ -94,7 +94,7 @@ module.exports.remove = async (req, res, next) => {
 
         const accessToRemoveMusic = await musicModel.findOne({ _id: id, user: user._id }).lean()
 
-        if (!accessToRemoveMusic || !user.role.includes("CONTENT-MODERATOR")) return response(res, 403, "You have no rigth to delete this music")
+        if (!accessToRemoveMusic || !user.role.includes("CONTENT-MODERATOR")) return response(res, 403, "You have no right to delete this music")
 
         const isExistMusic = await musicModel.findByIdAndDelete(id, "route covers poster").lean()
 
@@ -103,6 +103,63 @@ module.exports.remove = async (req, res, next) => {
         for (const cover of isExistMusic.covers) await fileDeleter('public', cover)
 
         return response(res, 200, "Deleting music successfully")
+    }
+    catch (error) {
+        next(error)
+    }
+}
+
+module.exports.update = async (req, res, next) => {
+    try {
+        const user = req.user;
+        const { id } = req.params;
+        const { musics, posters, covers } = req.files
+
+        if (!musics) {
+            await deleteFileHandler(req);
+            return response(res, 400, "upload music required");
+        }
+
+        req.body.poster = '/uploads' + '/posters/' + posters?.[0].filename.replace(/^\s+$/g, '');
+        req.body.route = '/uploads' + '/musics/' + musics?.[0]?.filename.replace(/^\s+$/g, '')
+        req.body.tags = req.body.tags.match(/#([\p{L}\p{N}_]+)/gu)
+
+        const removeMusic = await musicModel.findOneAndDelete({ _id: id, user: user._id }).lean()
+
+        if (!removeMusic || !user.role.includes("CONTENT-MODERATOR")) return response(res, 403, "You have no right to delete this music")
+
+        let check = await validateFiles(musics, allowedFormats.music, "musics", req);
+        if (!check.ok) return response(res, 422, check.message);
+
+        check = await validateFiles(posters, allowedFormats.poster, "posters", req);
+        if (!check.ok) return response(res, 422, check.message);
+
+        check = await validateFiles(covers, allowedFormats.covers, "covers", req);
+        if (!check.ok) return response(res, 422, check.message);
+
+        const coversArray = []
+        for (const file of covers) coversArray.push(`/uploads/covers/${file.filename.replace(/^\s+$/g, '')}`)
+
+        await fileDeleter('public', removeMusic.route)
+        await fileDeleter('public', removeMusic.poster)
+        for (const cover of removeMusic.covers) await fileDeleter('public', cover)
+
+        const upload = await musicModel.create({
+            ...req.body,
+            user: user._id,
+            covers: coversArray,
+        })
+
+        for (const tag of req.body.tags) {
+            await tagsModel.findOneAndUpdate({ title: tag }, {
+                $setOnInsert: { title: tag },
+                $push: {
+                    musics: upload._id
+                }
+            }, { new: true, upsert: true })
+        }
+
+        return response(res, 201, "Create new music successfully")
     }
     catch (error) {
         next(error)
